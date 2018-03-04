46c42f9b2dae875212a1803a96293819cc0da93c
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2018-03-05 03:32:24 +0700

    Simple working index page

---
+++ app/Main.elm
@@ -0,0 +1,136 @@
+import Html exposing (Html, div, text)
+import Array exposing (Array, get, set, repeat, fromList, toList, indexedMap, filter, map)
+import List exposing (member)
+
+main =
+    Html.beginnerProgram { model = initModel, view = view, update = update }
+
+
+-- Model
+
+type alias Model =
+    { gameState : GameState
+    , playerTurn : Int
+    , winner : Int
+    , allowedMoves : AllowedMoves
+    }
+
+type alias GameState = Array Int
+type alias AllowedMoves = Array Int
+
+initGameState : GameState
+initGameState =
+    repeat 9 0
+
+getAllowedMoves : GameState -> AllowedMoves
+getAllowedMoves game_state =
+    map extractIndex <| filter isNotMoved <| indexedMap (,) game_state
+
+isNotMoved : (Int, Int) -> Bool
+isNotMoved (_, state) =
+    if state == 0 then
+        True
+
+    else
+        False
+
+extractIndex : (Int, Int) -> Int
+extractIndex (index, _) =
+    index
+
+initModel : Model
+initModel =
+    let
+        gameState = initGameState
+        allowedMoves = getAllowedMoves gameState
+    in
+        { gameState = gameState
+        , playerTurn = 1
+        , winner = 0
+        , allowedMoves = allowedMoves
+        }
+
+-- Update
+
+type Msg = Move Int
+         | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Move move ->
+            if member move (toList model.allowedMoves) then
+                let
+                    newGameState = set move model.playerTurn model.gameState
+                    newPlayerTurn = model.playerTurn * -1
+                    newWinner = getWinner newGameState
+                    newAllowedMoves = getAllowedMoves newGameState
+                in
+                    { gameState = newGameState
+                    , playerTurn = newPlayerTurn
+                    , winner = newWinner
+                    , allowedMoves = newAllowedMoves
+                    }
+            else
+                model
+
+        Reset ->
+            initModel
+
+getWinner : GameState -> Int
+getWinner gameState =
+    let
+        winConditions =
+            [ [0, 1, 2]
+            , [3, 4, 5]
+            , [6 ,7, 8]
+            , [0, 3, 6]
+            , [1, 4, 7]
+            , [2, 5 ,8]
+            , [0, 4, 8]
+            , [2, 4, 6]
+            ]
+    in
+        gettingWinner gameState winConditions
+
+gettingWinner : GameState -> List (List Int) -> Int
+gettingWinner gameState winConditions =
+    case winConditions of
+        [] -> 0
+        (winningCondition::winningConditions) ->
+            let
+                winningArray = fromList winningCondition
+
+                a = getAt 0 winningArray
+                b = getAt 1 winningArray
+                c = getAt 2 winningArray
+
+                aValue = getAt a gameState
+                bValue = getAt b gameState
+                cValue = getAt c gameState
+
+                sumValue = aValue + bValue + cValue
+            in
+                case sumValue of
+                    3 -> 1
+                    (-3) -> -1
+                    _ -> gettingWinner gameState winningConditions
+
+getAt : Int -> Array Int -> Int
+getAt index array =
+    let
+        result = get index array
+    in
+        case result of
+            Just value ->
+                value
+
+            Nothing ->
+                0
+
+-- View
+
+view : Model -> Html msg
+view model =
+    div []
+        [ text "loaaaaaal" ]
